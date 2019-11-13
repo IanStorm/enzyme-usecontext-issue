2d7919f2f6d1c5d9c5b4a5f7cf68dd772297ee6e
Christian Sporer <christian.sporer@encoway.de>
2019-11-13 11:57:28 +0100

    Use Sinon in sandbox mode

--- src/project-view.spec.tsx
+++ src/project-view.spec.tsx
@@ -3,6 +3,7 @@ import Enzyme, { shallow } from "enzyme"
 import React16Adapter from "enzyme-adapter-react-16"
 import React from "react"
 import * as sinon from "sinon"
+import { SinonSandbox } from "sinon"
 import { Project } from "./project-context"
 import { ProjectView } from "./project-view"
 
@@ -12,12 +13,18 @@ describe("<ProjectView />", function() {
 
 	describe("'Set project' button", function() {
 
+		const sinonSandbox: SinonSandbox = sinon.createSandbox()
+
 		let mockProject: Project | undefined
 
 		beforeEach(function() { mockProject = { name: "Mocked project name" } })
 
+		afterEach(function() {
+			sinonSandbox.restore()
+		})
+
 		it("initializes the project's name.", function() {
-			sinon.stub(React, "useContext").returns(mockProject)
+			sinonSandbox.stub(React, "useContext").returns(mockProject)
 			const projectView = shallow(<ProjectView />)
 
 			projectView.find("#set-project").simulate("click")
